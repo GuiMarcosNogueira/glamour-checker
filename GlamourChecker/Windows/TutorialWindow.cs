dac6accc9b1f8ffcddef2aca64945ff0bd2993e4
Guilherme Nogueira <gui.nogueira@gmail.com>
2026-06-12 18:33:43 -0300

    Feature: Refactor tutorial into paginated MVVM tour

--- GlamourChecker/Windows/TutorialWindow.cs
+++ GlamourChecker/Windows/TutorialWindow.cs
@@ -2,63 +2,54 @@ using System;
 using System.Numerics;
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Windowing;
+using GlamourChecker.ViewModels;
 
 namespace GlamourChecker.Windows;
 
 [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public class TutorialWindow : Window, IDisposable
 {
-    private readonly Configuration _config;
-    private int _currentStep = 1;
+    private readonly TutorialViewModel _viewModel;
 
-    public TutorialWindow(Configuration config) : base(Loc.Localize("Tutorial_Title", "Welcome to Glamour Checker!"), ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar)
+    public TutorialWindow(TutorialViewModel viewModel) : base(Loc.Localize("Tutorial_Title", "Welcome to Glamour Checker!"), ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar)
     {
-        _config = config;
+        _viewModel = viewModel;
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(450, 260),
-            MaximumSize = new Vector2(450, 260)
+            MinimumSize = new Vector2(500, 340),
+            MaximumSize = new Vector2(500, 340)
         };
     }
 
     public override void Draw()
     {
-        ImGui.TextWrapped(Loc.Localize("Tutorial_Step1", "This plugin helps you identify which items in your inventory are missing from your Glamour Dresser and Armoire."));
-        ImGui.Spacing();
+        string pageText = Loc.Localize($"Tutorial_Page{_viewModel.CurrentPage}_Text", "Loading...");
 
-        if (_currentStep >= 2)
+        ImGui.TextWrapped(pageText);
+
+        ImGui.SetCursorPosY(300);
+
+        if (_viewModel.CurrentPage > 1)
         {
-            ImGui.TextWrapped(Loc.Localize("Tutorial_Step2", "To get started, you MUST physically open the Glamour Dresser or Armoire in the game."));
-            ImGui.Spacing();
+            if (ImGui.Button(Loc.Localize("Tutorial_Btn_Prev", "Previous")))
+            {
+                _viewModel.PreviousPage();
+            }
+            ImGui.SameLine();
         }
 
-        if (_currentStep >= 3)
-        {
-            ImGui.TextWrapped(Loc.Localize("Tutorial_Step3", "Once opened, click the \"Read Dresser/Armoire\" button in the main window to scan your currently stored items."));
-            ImGui.Spacing();
-        }
-
-        if (_currentStep >= 4)
-        {
-            ImGui.TextWrapped(Loc.Localize("Tutorial_Step4", "You can then see which items are \"New Appearances\" (not stored) and which are \"Duplicates\" (already stored)."));
-            ImGui.Spacing();
-        }
-
-        ImGui.SetCursorPosY(220);
-
-        if (_currentStep < 4)
+        if (_viewModel.CurrentPage < _viewModel.TotalPages)
         {
             if (ImGui.Button(Loc.Localize("Tutorial_Btn_Next", "Next")))
             {
-                _currentStep++;
+                _viewModel.NextPage();
             }
         }
         else
         {
             if (ImGui.Button(Loc.Localize("Tutorial_Btn_Finish", "Got it, finish tutorial!")))
             {
-                _config.HasSeenTutorial = true;
-                _config.Save();
+                _viewModel.FinishTutorial();
                 IsOpen = false;
             }
         }
